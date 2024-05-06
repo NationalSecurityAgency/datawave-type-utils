e053bcb3bf69241068617b8137435923ed7348d5
foster33 <realfosty@gmail.com>
2024-05-06 13:37:01 +0000

    Introduce type descriptions into classes themselves

--- src/main/java/datawave/data/type/RawDateType.java
+++ src/main/java/datawave/data/type/RawDateType.java
@@ -1,11 +1,13 @@
 package datawave.data.type;
 
 import datawave.data.normalizer.Normalizer;
+import datawave.data.type.util.TypePrettyNameSupplier;
 
-public class RawDateType extends BaseType<String> {
+public class RawDateType extends BaseType<String> implements TypePrettyNameSupplier {
     
     private static final long serialVersionUID = 936566410691643144L;
     private static final long STATIC_SIZE = PrecomputedSizes.STRING_STATIC_REF * 2 + Sizer.REFERENCE;
+    private static final String DATA_DICTIONARY_TYPE_NAME = "Raw Date";
     
     public RawDateType() {
         super(Normalizer.RAW_DATE_NORMALIZER);
@@ -24,5 +26,10 @@ public class RawDateType extends BaseType<String> {
     @Override
     public long sizeInBytes() {
         return STATIC_SIZE + (2 * normalizedValue.length()) + (2 * delegate.length());
+    }
+    
+    @Override
+    public String getDataDictionaryTypeValue() {
+        return DATA_DICTIONARY_TYPE_NAME;
     }
 }
