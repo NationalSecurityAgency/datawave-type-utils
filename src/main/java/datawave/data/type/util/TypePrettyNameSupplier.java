5303965a31a96bc27120833e2c696491b101352a
foster33 <84727868+foster33@users.noreply.github.com>
2024-12-13 22:59:28 +0000

    Update the default value to Unknown

--- src/main/java/datawave/data/type/util/TypePrettyNameSupplier.java
+++ src/main/java/datawave/data/type/util/TypePrettyNameSupplier.java
@@ -3,5 +3,5 @@ package datawave.data.type.util;
 public interface TypePrettyNameSupplier {
     String getDataDictionaryTypeValue();
     
-    public static final String DEFAULT_DATA_DICTIONARY_NAME = "Default";
+    public static final String DEFAULT_DATA_DICTIONARY_NAME = "Unknown";
 }
