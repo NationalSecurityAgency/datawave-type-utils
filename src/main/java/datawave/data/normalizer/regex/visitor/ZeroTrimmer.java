bbabee70dfba081e02c209cb6c39e787a41d4b2a
hlgp <hlgp@users.noreply.github.com>
2025-04-15 01:22:48 +0000

    unused imports

--- src/main/java/datawave/data/normalizer/regex/visitor/ZeroTrimmer.java
+++ src/main/java/datawave/data/normalizer/regex/visitor/ZeroTrimmer.java
@@ -5,13 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.w3c.dom.NodeList;
 
 import datawave.data.normalizer.ZeroRegexStatus;
 import datawave.data.normalizer.regex.AnyCharNode;
 import datawave.data.normalizer.regex.EncodedPatternNode;
 import datawave.data.normalizer.regex.EscapedSingleCharNode;
-import datawave.data.normalizer.regex.ExpressionNode;
 import datawave.data.normalizer.regex.GroupNode;
 import datawave.data.normalizer.regex.IntegerNode;
 import datawave.data.normalizer.regex.IntegerRangeNode;
